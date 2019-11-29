d534c317730b228aef6a9e0bccd78d008768d1b4
AvinashTiwari85 <qwe123kids@gmail.com>
2019-11-28 17:31:20 -0700

    no message

--- Chap1/proj/cards/lib/cards.ex
+++ Chap1/proj/cards/lib/cards.ex
@@ -20,4 +20,28 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filname) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filname, binary)
+  end
+
+  @spec load(
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | char,
+              binary | []
+            )
+        ) :: any
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok ->
+        :erlang.binary_to_term(binary)
+
+      :error ->
+        "File Doesnot Exist"
+    end
+  end
 end
