526705ab137a5a415a38d6c96a25dabaf979f8ef
AvinashTiwari85 <qwe123kids@gmail.com>
2019-12-04 19:04:22 -0700

    no message

--- Chap1/proj/cards/test/cards_test.exs
+++ Chap1/proj/cards/test/cards_test.exs
@@ -1,10 +1,6 @@
 defmodule CardsTest do
   use ExUnit.Case
-  doctest Cards
-
-  test "greets the world" do
-    assert Cards.hello() == :world
-  end
+  # doctest Cards
 
   test "the truth" do
     assert 1 + 1 == 2
