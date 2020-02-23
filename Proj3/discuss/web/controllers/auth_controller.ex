a9f6598a2caa463b8d816613eaa9638214a34bd0
AvinashTiwari85 <qwe123kids@gmail.com>
2020-02-23 12:03:42 -0700

    no message

--- Proj3/discuss/web/controllers/auth_controller.ex
+++ Proj3/discuss/web/controllers/auth_controller.ex
@@ -1,8 +1,11 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug(Ueberauth)
+  alias Discuss.User
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     IO.inspect(auth)
+    user_param = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeSet = User.changeset(%User{}, user_param)
   end
 end
