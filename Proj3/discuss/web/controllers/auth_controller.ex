b997f67120432953968a1f81406e7ad8739f026d
AvinashTiwari85 <qwe123kids@gmail.com>
2020-02-23 11:51:16 -0700

    no message

--- Proj3/discuss/web/controllers/auth_controller.ex
+++ Proj3/discuss/web/controllers/auth_controller.ex
@@ -2,6 +2,7 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug(Ueberauth)
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    IO.inspect(auth)
   end
 end
