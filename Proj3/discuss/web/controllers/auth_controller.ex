42e5bc8406c6588b21aebe14df779262bdf6a5b1
AvinashTiwari85 <qwe123kids@gmail.com>
2020-02-24 18:36:12 -0700

    no message

--- Proj3/discuss/web/controllers/auth_controller.ex
+++ Proj3/discuss/web/controllers/auth_controller.ex
@@ -7,5 +7,13 @@ defmodule Discuss.AuthController do
     IO.inspect(auth)
     user_param = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
     changeSet = User.changeset(%User{}, user_param)
+    insert_or_update_user(changeSet)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil -> Repo.insert(changeset)
+      user -> {:ok, user}
+    end
   end
 end
